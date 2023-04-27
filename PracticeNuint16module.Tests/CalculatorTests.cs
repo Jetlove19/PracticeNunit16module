b4c6d10f7b25b289a356ec19bcbf7d824cb9c7b2
Jetlove19 <rbrorlove@gmail.com>
2023-04-27 13:58:24 +0300

    Добавьте файлы проекта.

---
+++ PracticeNuint16module.Tests/CalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using PracticeNunit16module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeNuint16module.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustAddNumbers()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+
+        [Test]
+        public void SubtractionMustMinusNumbers()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(50,100) == -50);
+        }
+
+        [Test]
+        public void MultiMustMultCorrect()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(4, 3) == 12);
+        }
+        [Test]
+        public void DivideMustDivideCorrectNumbers() 
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(100, 10) == 10);
+        }
+    }
+}
---
+++ PracticeNuint16module.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeNuint16module.Tests
+{
+    public class Class1
+    {
+    }
+}
